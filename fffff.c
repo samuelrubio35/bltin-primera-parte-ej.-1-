0cd56b0c39f8e8d786cb71b399ce2a7c06cb0c16
Samuel <115562738+samuelrubio35@users.noreply.github.com>
2022-10-31 11:46:52 +0100

    Add files via upload

---
+++ fffff.c
@@ -0,0 +1,36 @@
+/*
+  // Programa de ** Prueba **
+
+
+void main (int) {
+
+floatvalor_real;
+
+printf(“Introduzca un carácter:”)
+scanf(“%hd”,caracter);
+
+printf(“El valor introducido es %hd” caracter)
+
+printf(“Introduzca un valor entero”);
+ scanf(“%f”, valor_entero);
+printf(“El valor introducido es %f” valor_entero);
+
+*/
+
+#include<stdio.h
+#include<stdlib.h>
+
+int main (){
+
+    float valor_real, valor_entero, caracter;
+
+    printf("Introduzca un caracter: ");
+    scanf("%f", &caracter);
+
+    if (caracter >0){
+        printf(“El valor introducido es %f”, caracter);
+    }else
+
+ return 0;
+
+}
